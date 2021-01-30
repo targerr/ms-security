dfdc6197accfdae76c406b1bf2e4fa1e1e1975fa
wgs <wgshuaiit@163.com>
2021-01-31 00:00:29 +0800

    密码加盐

--- chapter1/src/main/java/com/wanggs/pojo/User.java
+++ chapter1/src/main/java/com/wanggs/pojo/User.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import org.springframework.beans.BeanUtils;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
@@ -15,6 +17,7 @@ import javax.persistence.Id;
 @Entity
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String userName;
--- chapter1/src/main/java/com/wanggs/service/impl/UserServiceImpl.java
+++ chapter1/src/main/java/com/wanggs/service/impl/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.wanggs.service.impl;
 
+import com.lambdaworks.crypto.SCryptUtil;
 import com.wanggs.pojo.User;
 import com.wanggs.repository.UserRepository;
 import com.wanggs.service.UserService;
 import com.wanggs.vo.UserInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,7 @@ import java.util.List;
  * @Date: 2021/1/30
  */
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
@@ -34,7 +38,13 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserInfo save(UserInfo user) {
-        return null;
+    public UserInfo save(UserInfo info) {
+        User user = new User();
+        BeanUtils.copyProperties(info,user);
+        String saltPassword = SCryptUtil.scrypt(user.getPassword(),32768,8,1);
+        log.info("加密密码: " + saltPassword);
+        user.setPassword(saltPassword);
+        userRepository.save(user);
+        return info;
     }
 }
