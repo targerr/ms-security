1aa538ee0b505adf07c498f38dd12c6430d08028
wgs <wgshuaiit@163.com>
2021-02-09 18:18:03 +0800

    sentinel测试限流熔断

--- chapter3/is-order-api/src/main/java/com/wanggs/security/order/OrderController.java
+++ chapter3/is-order-api/src/main/java/com/wanggs/security/order/OrderController.java
@@ -1,5 +1,9 @@
 package com.wanggs.security.order;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -27,27 +31,42 @@ public class OrderController {
 
     /**
      * 第二版本
+     * 创建订单
      *
      * @param info
      * @param username
      * @return
      */
-    @PostMapping
+
     //注解生效需在启动类配置@EnableGlobalMethodSecurity(prePostEnabled = true)
-//    @PreAuthorize("#oauth2.hasScope('write')")
+    //@PreAuthorize("#oauth2.hasScope('write')")
+
+    @SentinelResource(value = "createOrder",blockHandler = "doOnBlock") //通过注解指定sentinel资源
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public OrderInfo create(@RequestBody OrderInfo info ,@AuthenticationPrincipal String username) {
-        log.info("获取到username = {}",username);
+    @PostMapping
+    public OrderInfo create(@RequestBody OrderInfo info, @AuthenticationPrincipal String username) throws InterruptedException {
+        Thread.sleep(50);
+        //查询价格
+        //PriceInfo price = restTemplate.getForObject("http://localhost:9080/prices/"+info.getProductId(),PriceInfo.class);
+        //log.info("price is "+price.getPrice());
         return info;
     }
 
 
+    //降级处理逻辑，和原方法在一个类里，参数一致，多一个BlockException，名字自定义
+    public OrderInfo doOnBlock(@RequestBody OrderInfo info,@AuthenticationPrincipal String username,BlockException exception) {
+        log.info("BlockException:{}",exception.getClass().getSimpleName());
+        info.setId(999L);
+        info.setProductId(999L);
+        return info;
+    }
+
     @GetMapping("/{id}")
-    public OrderInfo getInfo(@PathVariable Long id ,@AuthenticationPrincipal String username){
-        log.info("getInfo: id is "+id +" , and username is "+username);
+    public OrderInfo getInfo(@PathVariable Long id, @AuthenticationPrincipal String username) {
+        log.info("getInfo: id is " + id + " , and username is " + username);
         OrderInfo info = new OrderInfo();
         info.setId(id);
-        info.setProductId(id*10);
+        info.setProductId(id * 10);
         return info;
     }
 
